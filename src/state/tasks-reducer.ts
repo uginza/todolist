c973ca44f26686203c305087506034ef60553c05
uginza <uginza@yandex.ru>
2023-04-08 14:37:07 +0300

    Add Task in tasks-reducer.test.ts is set AddTaskAC is set and done

--- src/state/tasks-reducer.ts
+++ src/state/tasks-reducer.ts
@@ -7,26 +7,38 @@ type RemoveTaskActionType = {
     todolistId:string
 
 }
-type Action2Type = {
-    type: '2'
-    title: string
+type AddTaskActionType = {
+    type: 'ADD-TASK'
+    todolistTitle: string
+    todolistId:string
 }
 
 type ActionType = RemoveTaskActionType
-    | Action2Type
+    | AddTaskActionType
 
 
 
 export const tasksReducer = (state: TasksStateType, action: ActionType): TasksStateType => {
     switch (action.type) {
-        case 'REMOVE-TASK':
+        case 'REMOVE-TASK':{
             const stateCopy={...state}
             const tasks=stateCopy[action.todolistId]
             const filteredTasks=tasks.filter(t=>t.id!==action.taskId)
             stateCopy[action.todolistId]=filteredTasks
             return stateCopy
-        case '2':
-            return {...state}
+        }
+        case 'ADD-TASK':{
+            const stateCopy={...state}
+            const tasks=stateCopy[action.todolistId]
+            const newTask={id: v1(), title: action.todolistTitle, isDone: false}
+            const newTasks=[newTask,...tasks]
+            stateCopy[action.todolistId]=newTasks
+            return stateCopy
+            /*return{...state,[action.todolistId]:[{id: v1(),
+                    title: action.todolistTitle, isDone: false},
+                    ...state[action.todolistId]]}*/
+        }
+
         default:
             return state;
     }
@@ -35,6 +47,6 @@ export const tasksReducer = (state: TasksStateType, action: ActionType): TasksSt
 export const removeTaskAC=(taskId:string,todolistId:string):RemoveTaskActionType=>{
     return {type:'REMOVE-TASK',taskId,todolistId}
 }
-export const Action2Type=(todolistTitle: string):Action2Type=>{
-    return {type:'2',title:todolistTitle}
+export const addTaskAC=(todolistTitle: string,todolistId:string):AddTaskActionType=>{
+    return {type:'ADD-TASK',todolistTitle,todolistId}
 }

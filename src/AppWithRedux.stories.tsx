706270806e40741d214684da8ddc1c9dfe1649f4
uginza <uginza@yandex.ru>
2023-04-15 20:33:59 +0300

    ReduxStoreeProviderDecorator decorator is set with state and set to AppWithRedux.stories.tsx

--- src/AppWithRedux.stories.tsx
+++ src/AppWithRedux.stories.tsx
@@ -1,18 +1,36 @@
-import React from "react";
-import {action} from "@storybook/addon-actions"
-import AppWithRedux from "./AppWithRedux";
-import {Provider} from "react-redux";
-import {store} from "./state/store";
-import {ReduxStoreeProviderDecorator} from "./stories/ReduxStoreeProviderDecorator";
+import React from 'react'
+import { Provider } from 'react-redux'
+import { combineReducers, createStore } from 'redux'
+import { v1 } from 'uuid'
+import {tasksReducer} from "./state/tasks-reducer";
+import {todolistsReducer} from "./state/todolists-reducer";
+import {AppRootState} from "./state/store";
 
-const onChangeCallback = action('Value was removed')
+const rootReducer = combineReducers({
+    tasks: tasksReducer,
+    todolists: todolistsReducer
+})
 
-export default {
-    title: "AppWithRedux Component",
-    component: AppWithRedux,
-    decorators:[ReduxStoreeProviderDecorator]
+const initialGlobalState = {
+    todolists: [
+        {id: 'todolistId1', title: 'What to learn', filter: 'all'},
+        {id: 'todolistId2', title: 'What to buy', filter: 'all'}
+    ],
+    tasks: {
+        ['todolistId1']: [
+            {id: v1(), title: 'HTML&CSS', isDone: true},
+            {id: v1(), title: 'JS', isDone: true}
+        ],
+        ['todolistId2']: [
+            {id: v1(), title: 'Milk', isDone: true},
+            {id: v1(), title: 'React Book', isDone: true}
+        ]
+    }
 }
 
-export const AppWithReduxExample = () => {
-    return <AppWithRedux/>
-}
+export const storyBookStore = createStore(rootReducer, initialGlobalState as AppRootState)
+
+export const ReduxStoreProviderDecorator = (storyFn: any) => (
+    <Provider
+        store={storyBookStore}>{storyFn()}
+    </Provider>)

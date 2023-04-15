b06d3da7361d23cdbed3ea1734926f008d8a8e10
uginza <uginza@yandex.ru>
2023-04-15 20:50:43 +0300

    ReduxStoreProviderDecorator is fixed

--- src/AppWithRedux.stories.tsx
+++ src/AppWithRedux.stories.tsx
@@ -1,36 +1,18 @@
-import React from 'react'
-import { Provider } from 'react-redux'
-import { combineReducers, createStore } from 'redux'
-import { v1 } from 'uuid'
-import {tasksReducer} from "./state/tasks-reducer";
-import {todolistsReducer} from "./state/todolists-reducer";
-import {AppRootState} from "./state/store";
+import React from "react";
+import {action} from "@storybook/addon-actions"
+import AppWithRedux from "./AppWithRedux";
+import {Provider} from "react-redux";
+import {store} from "./state/store";
+import {ReduxStoreProviderDecorator} from "./stories/ReduxStoreProviderDecorator";
 
-const rootReducer = combineReducers({
-    tasks: tasksReducer,
-    todolists: todolistsReducer
-})
+const onChangeCallback = action('Value was removed')
 
-const initialGlobalState = {
-    todolists: [
-        {id: 'todolistId1', title: 'What to learn', filter: 'all'},
-        {id: 'todolistId2', title: 'What to buy', filter: 'all'}
-    ],
-    tasks: {
-        ['todolistId1']: [
-            {id: v1(), title: 'HTML&CSS', isDone: true},
-            {id: v1(), title: 'JS', isDone: true}
-        ],
-        ['todolistId2']: [
-            {id: v1(), title: 'Milk', isDone: true},
-            {id: v1(), title: 'React Book', isDone: true}
-        ]
-    }
+export default {
+    title: "AppWithRedux Component",
+    component: AppWithRedux,
+    decorators:[ReduxStoreProviderDecorator]
 }
 
-export const storyBookStore = createStore(rootReducer, initialGlobalState as AppRootState)
-
-export const ReduxStoreProviderDecorator = (storyFn: any) => (
-    <Provider
-        store={storyBookStore}>{storyFn()}
-    </Provider>)
+export const AppWithReduxExample = () => {
+    return <AppWithRedux/>
+}
--- src/stories/ReduxStoreeProviderDecorator.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import {Provider} from "react-redux";
-import {store} from "../state/store";
-
-export const  ReduxStoreeProviderDecorator=(storyFn:any)=>{
-    return <Provider store={store}>{storyFn()}</Provider>
-}

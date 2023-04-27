0a35f30292a8e5e0090a66f4e4d4d1304ce4582b
uginza <uginza@yandex.ru>
2023-04-27 20:46:19 +0300

    ReduxStoreProviderDecorator.tsx is fixed

--- src/stories/ReduxStoreProviderDecorator.tsx
+++ src/stories/ReduxStoreProviderDecorator.tsx
@@ -1,36 +1,139 @@
 import React from 'react'
-import { Provider } from 'react-redux'
-import { combineReducers, createStore } from 'redux'
-import { v1 } from 'uuid'
-import { AppRootState } from '../App/store'
-import { tasksReducer } from '../features/TodolistsList/Todolist/tasks-reducer'
-import { todolistsReducer } from '../features/TodolistsList/Todolist/todolists-reducer'
+import {Provider} from 'react-redux'
+import {combineReducers, createStore} from 'redux'
+import {v1} from 'uuid'
+import {AppRootState} from '../App/store'
+import {tasksReducer} from '../features/TodolistsList/Todolist/tasks-reducer'
+import {todolistsReducer} from '../features/TodolistsList/Todolist/todolists-reducer'
 import {TaskPriority, TaskStatus} from "../api/tasks-api";
+import {appReducer} from "../App/app-reducer";
 
 
 const rootReducer = combineReducers({
     tasks: tasksReducer,
-    todolists: todolistsReducer
+    todolists: todolistsReducer,
+    app: appReducer
 })
 
 const initialGlobalState = {
     todolists: [
-        {id: 'todolistId1', title: 'What to learn', filter: 'all', addedDate:'',order:0},
-        {id: 'todolistId2', title: 'What to buy', filter: 'all', addedDate:'',order:0}
+        {id: 'todolistId1', title: 'What to learn', filter: 'all', addedDate: '', order: 0},
+        {id: 'todolistId2', title: 'What to buy', filter: 'all', addedDate: '', order: 0}
     ],
-    tasks:{ ['todolistId1']: [
-    {id: v1(), title: "HTML&CSS",status:TaskStatus.Complited,addedDate:'',order:0,completed:true,deadline:'',todoListId:'todolistId1',startDate:'',description:'',priority:TaskPriority.Low},
-    {id: v1(), title: "JS", status:TaskStatus.Complited,order:0,completed:true,deadline:'',todoListId:'todolistId1',startDate:'',description:'',priority:TaskPriority.Low,addedDate:''},
-    {id: v1(), title: "ReactJS", status:TaskStatus.InProgress,order:0,completed:false,deadline:'',todoListId:'todolistId1',startDate:'',description:'',priority:TaskPriority.Low,addedDate:''},
-    {id: v1(), title: "rest API", status:TaskStatus.InProgress,order:0,completed:false,deadline:'',todoListId:'todolistId1',startDate:'',description:'',priority:TaskPriority.Low,addedDate:''},
-    {id: v1(), title: "GraphQL", status:TaskStatus.InProgress,order:0,completed:false,deadline:'',todoListId:'todolistId1',startDate:'',description:'',priority:TaskPriority.Low,addedDate:''}
-],
-    ['todolistId2']: [
-    {id: v1(), title: "Milk", status:TaskStatus.Complited,order:0,completed:true,deadline:'',todoListId:'todolistId2',startDate:'',description:'',priority:TaskPriority.Low,addedDate:''},
-    {id: v1(), title: "Bread", status:TaskStatus.Complited,order:0,completed:true,deadline:'',todoListId:'todolistId2',startDate:'',description:'',priority:TaskPriority.Low,addedDate:''},
-    {id: v1(), title: "Pen", status:TaskStatus.InProgress,order:0,completed:true,deadline:'',todoListId:'todolistId2',startDate:'',description:'',priority:TaskPriority.Low,addedDate:''}
-]
-}
+    tasks: {
+        ['todolistId1']: [
+            {
+                id: v1(),
+                title: "HTML&CSS",
+                status: TaskStatus.Complited,
+                addedDate: '',
+                order: 0,
+                completed: true,
+                deadline: '',
+                todoListId: 'todolistId1',
+                startDate: '',
+                description: '',
+                priority: TaskPriority.Low
+            },
+            {
+                id: v1(),
+                title: "JS",
+                status: TaskStatus.Complited,
+                order: 0,
+                completed: true,
+                deadline: '',
+                todoListId: 'todolistId1',
+                startDate: '',
+                description: '',
+                priority: TaskPriority.Low,
+                addedDate: ''
+            },
+            {
+                id: v1(),
+                title: "ReactJS",
+                status: TaskStatus.InProgress,
+                order: 0,
+                completed: false,
+                deadline: '',
+                todoListId: 'todolistId1',
+                startDate: '',
+                description: '',
+                priority: TaskPriority.Low,
+                addedDate: ''
+            },
+            {
+                id: v1(),
+                title: "rest API",
+                status: TaskStatus.InProgress,
+                order: 0,
+                completed: false,
+                deadline: '',
+                todoListId: 'todolistId1',
+                startDate: '',
+                description: '',
+                priority: TaskPriority.Low,
+                addedDate: ''
+            },
+            {
+                id: v1(),
+                title: "GraphQL",
+                status: TaskStatus.InProgress,
+                order: 0,
+                completed: false,
+                deadline: '',
+                todoListId: 'todolistId1',
+                startDate: '',
+                description: '',
+                priority: TaskPriority.Low,
+                addedDate: ''
+            }
+        ],
+        ['todolistId2']: [
+            {
+                id: v1(),
+                title: "Milk",
+                status: TaskStatus.Complited,
+                order: 0,
+                completed: true,
+                deadline: '',
+                todoListId: 'todolistId2',
+                startDate: '',
+                description: '',
+                priority: TaskPriority.Low,
+                addedDate: ''
+            },
+            {
+                id: v1(),
+                title: "Bread",
+                status: TaskStatus.Complited,
+                order: 0,
+                completed: true,
+                deadline: '',
+                todoListId: 'todolistId2',
+                startDate: '',
+                description: '',
+                priority: TaskPriority.Low,
+                addedDate: ''
+            },
+            {
+                id: v1(),
+                title: "Pen",
+                status: TaskStatus.InProgress,
+                order: 0,
+                completed: true,
+                deadline: '',
+                todoListId: 'todolistId2',
+                startDate: '',
+                description: '',
+                priority: TaskPriority.Low,
+                addedDate: ''
+            }
+        ]
+    },
+    app: {
+        status: 'idle',
+        error: null
+    }
 }
 
 export const storyBookStore = createStore(rootReducer, initialGlobalState as AppRootState)

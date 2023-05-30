1781b0123ba8dd07546156b16d82f47d67d49bb7
uginza <uginza@yandex.ru>
2023-05-30 14:53:25 +0300

    gh-pages is added

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import './index.css';
 import App from "./App/App";
 import {Provider} from "react-redux";
 import {store} from "./App/store";
-import AppWithReducers from "./temp/AppWithReducers";
 
 
 const root = ReactDOM.createRoot(
@@ -13,7 +12,7 @@ const root = ReactDOM.createRoot(
 root.render(
     <React.StrictMode>
         <Provider store={store}>
-            <AppWithReducers />
+            <App />
         </Provider>
     </React.StrictMode>
 
